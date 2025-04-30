a1bd2475f37d3f54335b88e5363f7619a2c20e86
Junaidkhokhar583 <ja.ahmed741@gmail.com>
2025-04-30 23:11:33 +0500

    formated home page

--- myMedApp/app/page.js
+++ myMedApp/app/page.js
@@ -1,21 +1,17 @@
-"use client"
+"use client";
 import React from "react";
 import HeroHeader from "./components/HeroHeader.jsx";
 import DescriptionCard from "./components/DescriptionCard.jsx";
 import HeroCard from "./components/HeroCard.jsx";
-import Button from './components/Button';
+import Button from "./components/Button";
 import Header from "./components/Header.jsx";
 import BaseFooter from "./components/BaseFooter.jsx";
 import GeneralFooter from "./components/GeneralFooter.jsx";
 
-
-
 export default function Home() {
-  
   return (
     <div className="bg-[#f2f7fa] xsm:w-[130%] exmd:w-full">
-      
-      <Header/>
+      <Header />
       <HeroHeader />
 
       <DescriptionCard
@@ -31,8 +27,7 @@ export default function Home() {
           "Convenient and Reliable Mobile Drug Testing Services",
           "Comprehensive Testing for RSV, FLU A & B viruses",
         ]}
-        src={["/covid-19.png","/Mobile.png","/RSV.png"]}
-       
+        src={["/covid-19.png", "/Mobile.png", "/RSV.png"]}
       />
 
       <HeroCard
@@ -44,8 +39,8 @@ export default function Home() {
           "Our mobile vans are equipped with the latest medical technology, ensuring top-quality care.",
           "Save time and effort with our convenient mobile healthcare services brought to your location.",
         ]}
-        src={[null,"/assets/amb.jpg"]}
-        style={{backgroundColor: "#e9f5ff" }}
+        src={[null, "/assets/amb.jpg"]}
+        style={{ backgroundColor: "#e9f5ff" }}
         href="/About-Us"
       />
       <HeroCard
@@ -54,50 +49,60 @@ export default function Home() {
         detailsText="The Mobile Wellness Group has a rich history , starting as WeTestU.com and evolving into a leading provider of mobile health solutions.Our mission it to bring advanced medical tesing and wellness services directly to your doorstep,providing convenience,reliablity and comprehensive care."
         mainText=""
         subText=""
-        src={["/hero-background.jpg","/medical.png"]}
-        style={{backgroundColor: "#FAFAFA" , minHeight:"60vh"}}
+        src={["/hero-background.jpg", "/medical.png"]}
+        style={{ backgroundColor: "#FAFAFA", minHeight: "60vh" }}
         href="/About-Us"
       />
-      <div className='hero min-h-[70vh]'> 
-        <div className='hero-content flex-col lg:flex-row-reverse '>
-        <div className='max-w-[35rem] '>
-        <div className='' >
+      <div className="hero min-h-[70vh]">
+        <div className="hero-content flex-col lg:flex-row-reverse ">
+          <div className="max-w-[35rem] ">
+            <div className="">
+              <p className="text-xl pb-2">
+                Contact Us now to book your Appointment and Experience the
+                Convenience of our healthcare services.
+              </p>
+              <div className="pt-[6.4px] space-x-4">
+                <a href="/Contact-Us">
+                  <Button
+                    type="button"
+                    text="Contact"
+                    className="rounded-[4px] "
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      height: "38px",
+                      width: "102px",
+                    }}
+                    onClick={() => {}}
+                  />
+                </a>
+                <a href="/Services">
+                  <Button
+                    type="button"
+                    text="Learn More"
+                    className="rounded-[4px] border-solid border-2 border-black "
+                    style={{
+                      backgroundColor: "white",
+                      color: "black",
+                      height: "38px",
+                      width: "102px",
+                    }}
+                    onClick={() => {}}
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
 
-        <p className='text-xl pb-2'>Contact Us now to book your Appointment and Experience the Convenience of our healthcare services.</p>
-        <div className='pt-[6.4px] space-x-4'>
-<a href="/Contact-Us">
-        <Button
-         type='button'
-         text='Contact'
-         className='rounded-[4px] '
-         style={{backgroundColor:'black',color:'white', height:'38px', width:'102px'}}
-         onClick={()=>{}}
-          />
-          </a> 
- <a href="/Services">
-        <Button
-         type='button'
-         text='Learn More'
-         className='rounded-[4px] border-solid border-2 border-black '
-         style={{backgroundColor:'white',color:'black', height:'38px', width:'102px'}}
-         onClick={()=>{}}
-         />
-         </a>
-         </div>
-         </div>
-         </div>
-
-         <div className='pr-[4.5rem]'>
-        <h3 className='text-5xl font-bold' >Schedule an Appointment Today!</h3>
-         </div>
-       
-
-         
-         </div>
+          <div className="pr-[4.5rem]">
+            <h3 className="text-5xl font-bold">
+              Schedule an Appointment Today!
+            </h3>
+          </div>
+        </div>
       </div>
-     <BaseFooter/>
-     <GeneralFooter/>
-    
+      <BaseFooter />
+      <GeneralFooter />
     </div>
   );
 }
